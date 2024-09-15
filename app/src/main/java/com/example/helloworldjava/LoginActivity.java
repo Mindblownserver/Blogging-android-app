45989e3368557d11278f6a6c391e285cfa95a600
Mindblownserver <med.yassine.kharrat@gmail.com>
2024-09-15 16:44:31 +0100

    Added working bottomNavBar and search fragment

--- app/src/main/java/com/example/helloworldjava/LoginActivity.java
+++ app/src/main/java/com/example/helloworldjava/LoginActivity.java
@@ -36,7 +36,9 @@ public class LoginActivity extends AppCompatActivity {
 
                     usernameInput.setText("");
                     passwordInput.setText("");
-                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
                 }else{
                     Toast.makeText(LoginActivity.this, "The Username or/and password are incorrect", Toast.LENGTH_SHORT).show();
                 }
--- app/src/main/java/com/example/helloworldjava/MainActivity.java
+++ app/src/main/java/com/example/helloworldjava/MainActivity.java
@@ -1,29 +1,30 @@
 package com.example.helloworldjava;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.helloworldjava.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 
 }
